e1224024b0685f2a4919ad156c9f458b0e4d37e9
Danil Filatov <danilfilat@gmail.com>
2024-12-13 17:25:35 +0500

    09.11

---
+++ Kurs.py
@@ -0,0 +1,9 @@
+number_of_completed_homework = 12
+
+number_of_hours_spent = 1.5
+
+Course_name = 'Python'
+
+time_for_one_task = 1.5/12
+
+print('Курс:',Course_name,',''всего задач',number_of_completed_homework,',''затрачено часов',number_of_hours_spent,',''среднее время выполнения',time_for_one_task)
