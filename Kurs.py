294636cf114c6fb504ac6dc754a651224243dec1
GreenIceDand <danilfilat@gmail.com>
2025-01-10 13:00:26 +0500

    Update Kurs.py

--- Kurs.py
+++ Kurs.py
@@ -4,6 +4,6 @@ number_of_hours_spent = 1.5
 
 Course_name = 'Python'
 
-time_for_one_task = 1.5/12
+time_for_one_task = number_of_hours_spent/number_of_completed_homework 
 
 print('Курс:',Course_name,',''всего задач',number_of_completed_homework,',''затрачено часов',number_of_hours_spent,',''среднее время выполнения',time_for_one_task)
